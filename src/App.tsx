9afd576ecfcbb1d36ced13324ee6888c400c3cf8
rickypatel18 <patelricky184@gmail.com>
2025-05-08 11:49:27 +0530

    add lenis library for smooth scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
+import ScrollContext from "./context/ScrollContext";
 
 const App = () => {
   return (
     <Router>
+      <ScrollContext>
       <AppRoutes />
+      </ScrollContext>
     </Router>
   );
 };
